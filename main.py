e19a7d66f3d0c1a2c2451c2ba0df5d32fb871aca
Ilya Tsikhanionak <ilya.tihanyonok@yandex.by>
2024-12-07 12:13:09 +0300

    initial structure

---
+++ main.py
@@ -0,0 +1,28 @@
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ContextTypes,
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Привет! Я ваш новый бот.')
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Вы сказали: {update.message.text}')
+
+def main():
+    TOKEN = "7582758306:AAEO5Wl9kCoYhFQQu0Mo79WonYK6FZbhgSs"
+
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    app.run_polling()
+
+
+if __name__ == '__main__':
+    main()
